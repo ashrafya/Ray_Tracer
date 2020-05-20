b039af6aee40bcdc42b1a0b3e4f951413b9aa87e
Yawar Ashraf <yawar1ashraf@gmail.com>
2020-05-20 16:26:41 -0400

    refined implementation of sphere
    the ray tracer is working more efficiently now (efficiently)

--- code_implementation_projects/ch5_circle.py
+++ code_implementation_projects/ch5_circle.py
@@ -1,29 +1,32 @@
-from ray_sphere import ray, sphere
+from bases import point
 from canvas_color import color, canvas
-from bases import point, vector
-import time
+from sphere import sphere
+from ray import ray
 
-tic = time.time()
 ray_origin = point(0, 0, -5)
 wall_z = 10
+
 wall_size = 7
+
 canvas_pixels = 100
+
 pixel_size = wall_size/canvas_pixels
-half = wall_size / 2
+
+half = wall_size/2
+
+canv = canvas(canvas_pixels, canvas_pixels)
 red = color(255, 0, 0)
-canv = canvas(canvas_pixels, canvas_pixels, 255, 255, 255)
 shape = sphere()
 
 for y in range(canvas_pixels):
+    print('line {} of {}'.format(y+1, canvas_pixels))
     world_y = half - pixel_size * y
     for x in range(canvas_pixels):
         world_x = -half + pixel_size * x
         position = point(world_x, world_y, wall_z)
-        position = position - ray_origin
-        r = ray(ray_origin, position.normalize())
+        r = ray(ray_origin, (position-ray_origin).normalize())
         xs = shape.intersect(r)
-        if xs != []:
+        if xs:
             canv.write_canvas(x, y, red)
-canv.to_ppm(filename='japan_flag')
-toc = time.time()
-print(round(toc-tic, 4))
+
+canv.to_ppm(filename='ch5_test')
